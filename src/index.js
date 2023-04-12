b424690d7373c3e54a816c6f70e6377c60fa208b
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-04-12 12:52:16 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,52 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
+const list = [
+  {
+    id: '1',
+    name: 'name',
+    score: '100',
+  },
+  {
+    id: '2',
+    name: 'name',
+    score: '20',
+  },
+  {
+    id: '3',
+    name: 'name',
+    score: '50',
+  },
+  {
+    id: '4',
+    name: 'name',
+    score: '78',
+  },
+  {
+    id: '5',
+    name: 'name',
+    score: '125',
+  },
+  {
+    id: '6',
+    name: 'name',
+    score: '77',
+  },
+  {
+    id: '7',
+    name: 'name',
+    score: '42',
+  },
+];
+
+const listItem = document.getElementById('list');
+
+list.map((item) => {
+  if (item.id % 2 === 0) {
+    listItem.innerHTML += `
+    <li class= 'bgGrey'>${item.name}:<span>${item.score}</span></li>`;
+  } else {
+    listItem.innerHTML += `
+<li>${item.name}:<span>${item.score}</span></li>`;
   }
-  
-  document.body.appendChild(component());
+  return ('');
+});
